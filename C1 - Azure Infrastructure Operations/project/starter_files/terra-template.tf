2cd41ad327227a6a3d51856b00b5091d28b05a3e
yusufbagci <bagciy16@itu.edu.tr>
2022-08-03 13:39:12 +0200

    enter

--- C1 - Azure Infrastructure Operations/project/starter_files/terra-template.tf
+++ C1 - Azure Infrastructure Operations/project/starter_files/terra-template.tf
@@ -6,3 +6,16 @@ resource "azurerm_resource_group" "main" {
   name     = "${var.prefix}-resources"
   location = var.location
 }
+
+resource "azurerm_virtual_network" "main" {
+  name                = "${var.prefix}-network"
+  address_space       = ["10.0.0.0/22"]
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+}
+
+resource "azurerm_subnet" "internal" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.0.2.0/24"]
